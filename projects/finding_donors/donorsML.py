99ca2b89e89f0236164a7d6f54a11027abddf110
Konstantin <kostyameskhidze@gmail.com>
2017-11-04 10:00:34 +0300

    Project Donors ML
    Project Donors ML: added 3 classifiers : GaussianNB, DecisionTreeClassifier, SVM

--- projects/finding_donors/donorsML.py
+++ projects/finding_donors/donorsML.py
@@ -13,11 +13,71 @@ from IPython.display import display # Allows the use of display() for DataFrames
 # Import sklearn.preprocessing.StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 
+# TODO: Import two metrics from sklearn - fbeta_score and accuracy_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import fbeta_score
+
+# TODO: Import the three supervised learning models from sklearn
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+
 # Import supplementary visualization code visuals.py
 import visuals as vs
 
 # Pretty display for notebooks
 #%matplotlib inline
+
+
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test): 
+    '''
+    inputs:
+       - learner: the learning algorithm to be trained and predicted on
+       - sample_size: the size of samples (number) to be drawn from training set
+       - X_train: features training set
+       - y_train: income training set
+       - X_test: features testing set
+       - y_test: income testing set
+    '''
+    
+    results = {}
+    
+    # TODO: Fit the learner to the training data using slicing with 'sample_sisample_sizeze' using .fit(training_features[:], training_labels[:])
+    start = time() # Get start time
+    learner.fit(X_train[:sample_size], y_train[:sample_size]['>50K'])
+    end = time() # Get end time
+    
+    # TODO: Calculate the training time
+    results['train_time'] = end -start
+        
+    # TODO: Get the predictions on the test set(X_test),
+    #       then get predictions on the first 300 training samples(X_train) using .predict()
+    start = time() # Get start time
+    predictions_test = learner.predict(X_test[:300])
+    predictions_train = learner.predict(X_train[:300])
+    end = time() # Get end time
+    
+    # TODO: Calculate the total prediction time
+    results['pred_time'] = end - start
+            
+    # TODO: Compute accuracy on the first 300 training samples which is y_train[:300]
+    results['acc_train'] = accuracy_score(y_train[:300]['>50K'], predictions_train[:300])
+        
+    # TODO: Compute accuracy on test set using accuracy_score()
+    results['acc_test'] = accuracy_score(y_test[:300]['>50K'], predictions_test[:300])
+    
+    # TODO: Compute F-score on the the first 300 training samples using fbeta_score()
+    results['f_train'] = fbeta_score(y_train[:300]['>50K'], predictions_train[:300], beta=0.5)
+         
+    # TODO: Compute F-score on the test set which is y_test
+    results['f_test'] = fbeta_score(y_test[:300]['>50K'], predictions_test[:300], beta=0.5)
+       
+    # Success
+    print "{} trained on {} samples.".format(learner.__class__.__name__, sample_size)
+        
+    # Return the results
+    return results
 
 # Load the Census dataset
 data = pd.read_csv("census.csv")
@@ -115,3 +175,35 @@ fscore = (1 + beta**2) * (precision * recall) / ((beta**2 * precision) + recall)
 
 # Print the results 
 print "Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore);
+
+
+
+# TODO: Initialize the three models
+clf_A = GaussianNB()
+clf_B = DecisionTreeClassifier()
+clf_C = svm.SVC()
+
+
+# TODO: Calculate the number of samples for 1%, 10%, and 100% of the training data
+# HINT: samples_100 is the entire training set i.e. len(y_train)
+# HINT: samples_10 is 10% of samples_100
+# HINT: samples_1 is 1% of samples_100
+samples_100 = len(y_train)
+samples_10 = len(y_train)/10
+samples_1 = len(y_train)/100
+
+# Collect results on the learners
+results = {}
+for clf in [clf_A, clf_B, clf_C]:
+    clf_name = clf.__class__.__name__
+    results[clf_name] = {}
+    for i, samples in enumerate([samples_1, samples_10, samples_100]):
+        results[clf_name][i] = \
+        train_predict(clf, samples, X_train, y_train, X_test, y_test)
+
+# Run metrics visualization for the three supervised learning models chosen
+vs.evaluate(results, accuracy, fscore)
+
+
+
+
