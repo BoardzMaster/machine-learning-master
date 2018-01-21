7094984e6c7237879b8131002c9b498939650755
Konstantin <kostyameskhidze@gmail.com>
2018-01-21 16:54:50 +0300

    CustomersML

---
+++ projects/customer_segments/customerML.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 19:37:47 2017
+
+@author: konstantin
+"""
+
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from IPython.display import display # Allows the use of display() for DataFrames
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+#%matplotlib inline
+
+# Load the wholesale customers dataset
+try:
+    data = pd.read_csv("customers.csv")
+    data.drop(['Region', 'Channel'], axis = 1, inplace = True)
+    print "Wholesale customers dataset has {} samples with {} features each.".format(*data.shape)
+except:
+    print "Dataset could not be loaded. Is the dataset missing?"
+    
+    # Display a description of the dataset
+display(data.describe())
+
+# TODO: Select three indices of your choice you wish to sample from the dataset
+indices = [1, 2, 3]
+
+# Create a DataFrame of the chosen samples
+samples = pd.DataFrame(data.loc[indices], columns = data.keys()).reset_index(drop = True)
+print "Chosen samples of wholesale customers dataset:"
+display(samples)
