bd19d5b67b11ef4bbcfa39b9af0e3f71fde115c2
Konstantin <kostyameskhidze@gmail.com>
2018-02-25 12:50:08 +0300

    CustomerML
    log data and samples

--- projects/customer_segments/customerML.py
+++ projects/customer_segments/customerML.py
@@ -9,6 +9,10 @@ Created on Thu Nov 30 19:37:47 2017
 import numpy as np
 import pandas as pd
 from IPython.display import display # Allows the use of display() for DataFrames
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+import seaborn as sns;
 
 # Import supplementary visualizations code visuals.py
 import visuals as vs
@@ -24,13 +28,41 @@ try:
 except:
     print "Dataset could not be loaded. Is the dataset missing?"
     
-    # Display a description of the dataset
+ # Display a description of the dataset
 display(data.describe())
 
 # TODO: Select three indices of your choice you wish to sample from the dataset
-indices = [1, 2, 3]
+indices = [46, 222, 412]
 
 # Create a DataFrame of the chosen samples
 samples = pd.DataFrame(data.loc[indices], columns = data.keys()).reset_index(drop = True)
 print "Chosen samples of wholesale customers dataset:"
 display(samples)
+
+
+# TODO: Make a copy of the DataFrame, using the 'drop' function to drop the given feature
+new_data =  data.drop('Grocery', axis=1)
+
+# TODO: Split the data into training and testing sets(0.25) using the given feature as the target
+# Set a random state.
+X_train, X_test, y_train, y_test = train_test_split(new_data, data['Grocery'], test_size=0.25, random_state=10)
+
+# TODO: Create a decision tree regressor and fit it to the training set
+regressor = DecisionTreeRegressor(random_state=5).fit(X_train, y_train)
+
+# TODO: Report the score of the prediction using the testing set
+score = r2_score(y_test, regressor.predict(X_test))
+
+# Produce a scatter matrix for each pair of features in the data
+pd.scatter_matrix(data, alpha = 0.3, figsize = (14,8), diagonal = 'kde');
+                 
+sns.heatmap(data.corr())       
+
+      # TODO: Scale the data using the natural logarithm
+log_data = np.log(data)
+
+# TODO: Scale the sample data using the natural logarithm
+log_samples = np.log(samples)
+
+# Produce a scatter matrix for each pair of newly-transformed features
+pd.scatter_matrix(log_data, alpha = 0.3, figsize = (14,8), diagonal = 'kde'); 
