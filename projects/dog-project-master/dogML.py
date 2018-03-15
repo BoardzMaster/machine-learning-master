7255c5e6bfbf8c2509d3cb35c05226a68181c776
Konstantin <kostyameskhidze@gmail.com>
2018-03-15 09:00:10 +0300

    DogML
    Defined Conv Net structure

--- projects/dog-project-master/dogML.py
+++ projects/dog-project-master/dogML.py
@@ -6,6 +6,18 @@ from glob import glob
 import cv2                
 import matplotlib.pyplot as plt      
 
+from keras.applications.resnet50 import ResNet50
+
+from keras.preprocessing import image                  
+from tqdm import tqdm
+
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+from PIL import ImageFile                            
+ImageFile.LOAD_TRUNCATED_IMAGES = True   
+
+
+
 # define function to load train, test, and validation datasets
 def load_dataset(path):
     data = load_files(path)
@@ -19,6 +31,28 @@ def face_detector(img_path):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
     return len(faces) > 0
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+### returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151)) 
 
 # load train, test, and validation datasets
 train_files, train_targets = load_dataset('dogImages/train')
@@ -82,4 +116,30 @@ for element in human_files_short:
 dog_found = 0;
 for element in dog_files_short:
     dog_found = dog_found + face_detector(element )
+    
+ # define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
 
+### TODO: Test the performance of the dog_detector function
+### on the images in human_files_short and dog_files_short.
+
+face_found = 0;
+for element in human_files_short:
+    face_found = face_found + dog_detector(element)
+
+Percentage_face_found = 100*face_found/len(human_files_short)
+print('Percentage face found' , Percentage_face_found,'%')
+
+
+dog_found = 0;
+for element in dog_files_short:
+    dog_found = dog_found + dog_detector(element)
+
+Percentage_dog_found = 100*dog_found/len(dog_files_short)
+print('Percentage dog found' , Percentage_dog_found,'%')
+              
+
+# pre-process the data for Keras
+train_tensors = paths_to_tensor(train_files).astype('float32')/255
+valid_tensors = paths_to_tensor(valid_files).astype('float32')/255
+test_tensors = paths_to_tensor(test_files).astype('float32')/255
